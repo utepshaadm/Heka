ce6fa27a1ef09014662e2061625188086de008eb
utepshaadm <utep.shaadm@gmail.com>
2026-09-14 13:02:02 -0700

    Heka refit final

--- heka.py
+++ heka.py
@@ -15,7 +15,7 @@ class HEKA:
          
          self.deck.insert(self.deck[1], self.deck.pop(0))
          
-         return self.deck[(self.deck[self.deck[0]] + 1) % 52] % 26
+         return self.deck[self.deck[self.deck[0]]] % 26
          
     def encrypt_letter(self, letter):
         num = ord(letter) - 65
